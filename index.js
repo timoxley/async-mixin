fb692eba1ce8984b0a4e7d3ca1c171521862fac4
Tim Oxley <secoif@gmail.com>
2016-08-10 02:40:17 +0800

    Use standard style.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var async = require('async')
 var slice = Function.call.bind(Array.prototype.slice)
 
-var asyncMixin = module.exports = function asyncMixin(target, property, methods) {
-  if (typeof target === 'string') methods = property, property = target, target = null
-  
+module.exports = function asyncMixin (target, property, methods) {
+  if (typeof target === 'string') {
+    methods = property
+    property = target
+    target = null
+  }
+
   if (Array.isArray(target)) {
     target = {
       items: arguments[0]
@@ -14,9 +18,9 @@ var asyncMixin = module.exports = function asyncMixin(target, property, methods)
   if (!Array.isArray(methods)) methods = asyncCollectionMethods
 
   var wrapper = Object.create(target || {})
-  methods.forEach(function(key) {
+  methods.forEach(function (key) {
     if (typeof async[key] !== 'function') return
-    wrapper[key] = function() {
+    wrapper[key] = function () {
       return async[key].apply(this, [this[property]].concat(slice(arguments)))
     }
   })
